95122645a03a87eff390467fc6aa9581136f33df
Cip <xeonsat@gmail.com>
2021-04-29 18:32:46 +0800

    Search and Dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BiHomeSmile } from "react-icons/bi"
+import Search from "./components/Search"
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl"><BiHomeSmile className="inline-block text-blue-400 align-top"/>Appointments App</h1>
+    <Search />
     </div>
   );
 }
