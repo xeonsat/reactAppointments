1703fee1bdfe4977fcb0d51f9f682ff1a972c3c1
Cip <xeonsat@gmail.com>
2021-04-29 18:41:58 +0800

    AddAppointment App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BiHomeSmile } from "react-icons/bi"
 import Search from "./components/Search"
+import AddAppointment from "./components/AddAppointment"
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl"><BiHomeSmile className="inline-block text-blue-400 align-top"/>Appointments App</h1>
+      <h1 className="text-5xl mb-3">
+        <BiHomeSmile className="inline-block text-blue-400 align-top"/>Appointments App</h1>
+    <AddAppointment />
     <Search />
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -49,4 +49,4 @@ const Search = () => {
     )
 }
 
-export default Search;
+export default Search
