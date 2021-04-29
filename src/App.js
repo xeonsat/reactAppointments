f6fb38e2199247a5b04e87cdfe5a346265ab36a6
Cip <xeonsat@gmail.com>
2021-04-29 18:53:35 +0800

    importing JSON data as a variable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BiHomeSmile } from "react-icons/bi"
+import { BiHomeSmile, BiTrash } from "react-icons/bi"
 import Search from "./components/Search"
 import AddAppointment from "./components/AddAppointment"
+import appointmentList from "./data.json"
 
 function App() {
   return (
@@ -9,6 +10,27 @@ function App() {
         <BiHomeSmile className="inline-block text-blue-400 align-top"/>Appointments App</h1>
     <AddAppointment />
     <Search />
+
+    <ul className="divide-y divide-gray-200">
+      {appointmentList
+        .map(appointment => (
+          <li className="px-3 py-3 flex items-start">
+            <button type="button"
+             className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+             <BiTrash /></button>
+          <div className="flex-grow">
+          <div className="flex items-center">
+          <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
+          <span className="flex-grow text-right">{appointment.aptDate}</span>
+          </div>
+          <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
+          <div className="leading-tight">{appointment.aptNotes}</div>
+          </div>
+          </li>
+        ))
+      }
+    </ul>
+
     </div>
   );
 }
